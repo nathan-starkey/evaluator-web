79f3d8b13118255153f985afafec7d809908b625
Nathan Starkey <40289071+nathan-starkey@users.noreply.github.com>
2026-03-29 11:05:42 +1300

    feat: add evaluation of sum, product and exponent

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log("Hello, World!");
+import { evaluateExpr } from "./evaluate.js";
+import { MathExpr } from "./types.js";
+
+const expr: MathExpr = {
+  type: "exponent",
+  base: {
+    type: "product",
+    factors: [
+      {
+        type: "integer",
+        value: 2,
+      },
+      {
+        type: "integer",
+        value: 3,
+      },
+    ],
+  },
+  power: {
+    type: "sum",
+    summands: [
+      {
+        type: "integer",
+        value: -3,
+      },
+      {
+        type: "integer",
+        value: 5,
+      },
+    ],
+  },
+};
+
+console.log(evaluateExpr(expr));
