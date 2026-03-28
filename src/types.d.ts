02b19624a6c669d5b2ac011b207762eeac8efeaa
Nathan Starkey <40289071+nathan-starkey@users.noreply.github.com>
2026-03-29 10:24:02 +1300

    feat: add math expression types

---
+++ src/types.d.ts
@@ -0,0 +1,42 @@
+export type Summand = Exclude<MathExpr, Sum>;
+
+export type Factor = Exclude<MathExpr, Product>;
+
+export type Undefined = {
+  type: "undefined";
+};
+
+export type Integer = {
+  type: "integer";
+  value: number;
+};
+
+export type Sum = {
+  type: "sum";
+  summands: Summand[];
+};
+
+export type Product = {
+  type: "product";
+  factors: Factor[];
+};
+
+export type Fraction = {
+  type: "fraction";
+  numerator: MathExpr;
+  denominator: MathExpr;
+};
+
+export type Exponent = {
+  type: "exponent";
+  base: MathExpr;
+  power: MathExpr;
+};
+
+export type MathExpr =
+  | Undefined
+  | Integer
+  | Sum
+  | Product
+  | Fraction
+  | Exponent;
